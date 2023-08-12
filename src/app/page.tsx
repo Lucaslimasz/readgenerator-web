7e901da9a8977df535705af4158dc18fe5593ccf
lucaslima <id.lucas@outlook.com.br>
2023-08-12 00:03:56 -0300

    integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,81 +1,46 @@
 'use client'
-import { useState } from "react";
-
-interface IInfo {
-  images: string;
-  title: string;
-  subtitle: string;
-  description: string;
-  link: string;
-  technologiesUsed: string;
-  github: string;
-  tutorialToStartProject: string;
-  user: string;
-}
+import { api } from "@/config/api";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
   const [isStart, setIsStart] = useState<boolean>(false);
-  const [info, setInfo] = useState<IInfo>({} as IInfo);
+  const [info, setInfo] = useState<any>([]);
   const [valueInput, setValueInput] = useState<string>('');
-
+  const [questions, setQuestions] = useState<any>([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
 
-  const data = [
-    {
-      id: 'title',
-      question: 'Qual o título do projeto?',
-    },
-    {
-      id: 'sub-title',
-      question: 'Qual o sub-título do projeto?',
-    },
-    {
-      id: 'description-project',
-      question: 'Descrição do projeto',
-    },
-    {
-      id: Math.random(),
-      question: 'Qual o link do projeto no ar?',
-    },
-    {
-      id: Math.random(),
-      question: 'Quais tecnologias foram usadas no projeto? (Separe por vírgulas)',
-    },
-    {
-      id: Math.random(),
-      question: 'Qual o link do repositório no GitHub?',
-    },
-    {
-      id: Math.random(),
-      question: 'Qual é o tutorial para iniciar o projeto?',
-    },
-    {
-      id: Math.random(),
-      question: 'Qual o seu nome de usuário no github?',
-    },
-  ];
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('/questions')
+      setQuestions(data.data);
+    })()
+  }, []);
 
   const handleStart = () => {
     setIsStart(true);
   };
 
   const handleInputChange = (value: any) => {
-    const newInfo: IInfo = { ...info };
-    let currentQuestion = data[currentQuestionIndex].question.toLowerCase();
-    newInfo[currentQuestion as keyof IInfo] = value;
-
-    console.log(currentQuestion, newInfo)
-
-    setInfo(newInfo);
+    let currentQuestion = questions[currentQuestionIndex].id.toLowerCase();
+    const inf = { id: currentQuestion, answer: value }
+    setInfo((oldState: any) => ([...oldState, inf]));
   };
 
-
-  const handleInputKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleInputKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      if (currentQuestionIndex < data.length - 1) {
+      if (currentQuestionIndex < questions.length - 1) {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
         handleInputChange(valueInput)
         setValueInput('')
+      } else {
+        const { data } = await api.post('/questions', info);
+        const blob = new Blob([data], { type: 'text/markdown' });
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'README.md';
+        a.click();
+        window.URL.revokeObjectURL(url);
       }
     }
   };
@@ -94,8 +59,8 @@ export default function Home() {
 
       {
         !isStart && (
-          <button onClick={handleStart} className="bg-[#180b2c] py-4 px-8 rounded-md font-bold">
-            Iniciar
+          <button onClick={handleStart} className="bg-[#180b2c] border-purple-950 border-2 py-4 px-10 rounded-full font-bold">
+            Gerar ReadMe
           </button>
         )
       }
@@ -104,12 +69,12 @@ export default function Home() {
         isStart && (
           <div className="flex flex-col w-full items-center">
             <div className="w-full m-auto justify-center items-center max-w-xl">
-              <p className="text-lg text-slate-300 m-auto">{data[currentQuestionIndex].question}</p>
+              <p className="text-lg text-slate-300 m-auto">{questions[currentQuestionIndex].question}</p>
               <input
                 className="bg-transparent w-full border-b-violet-700 border-b-[1px] mt-5 pb-4 text-2xl outline-0"
                 placeholder="Responda aqui"
                 onChange={(e) => setValueInput(e.target.value)}
-                onKeyPress={handleInputKeyPress}
+                onKeyUp={handleInputKeyPress}
                 value={valueInput}
               />
               <p className="mt-2 text-slate-500"><b className="text-slate-200">Enter</b> para continuar</p>
