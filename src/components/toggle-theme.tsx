ac7f628f6c38bad6f0590c54f4b7fc876f60e9a6
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-13 16:03:04 -0300

    feat: added toggle theme component

---
+++ src/components/toggle-theme.tsx
@@ -0,0 +1,24 @@
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { BsSun } from "react-icons/bs";
+import { BsMoonStars } from "react-icons/bs";
+
+export const ToggleTheme = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
+
+    const handleThemeSwitch = () => setTheme(theme === 'dark' ? 'light' : 'dark');
+
+    return (
+        <button onClick={handleThemeSwitch} title="Mudar tema">
+            {theme === 'dark' ?
+                <BsSun className="w-8 h-8 text-gray-800 dark:text-white" /> :
+                <BsMoonStars className="w-8 h-8 text-gray-800 dark:text-white" />
+            }
+        </button>
+    )
+}
