f5fe7821bb2b4e216106a3b3086de5d0b38d80a6
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-13 20:38:23 -0300

    Revert "refactor: using theme provider and fixing the name"
    This reverts commit bdb8a02b01eb8844bad1e03fdd8beea63348ca41.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-import "./globals.css";
+import './globals.css'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-import Providers from "@/components/providers";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
