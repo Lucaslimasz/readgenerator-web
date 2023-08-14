09d8d3a9ecc4c6984de7ae5bedec0ebaa9e8d945
Alexander Davis <alexander.davis.098@gmail.com>
2023-08-13 23:11:01 -0300

    fix: using provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Providers from '@/components/providers'
 import './globals.css'
 
 import type { Metadata } from 'next'
@@ -17,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
